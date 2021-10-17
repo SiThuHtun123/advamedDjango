7f1d4aaabdd80b64d3d089d42633547234a200e0
Si Thu Htun <chekoilar@gmail.com>
2021-10-17 12:50:07 +0630

    Updated

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="admin")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
